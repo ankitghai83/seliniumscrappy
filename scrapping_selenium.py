7b9db68adf4473fd839b0110d713d0c45e0dbb3e
ankitghai83 <ankit.ghai@gmail.com>
2024-09-14 11:23:17 +0530

    final

--- scrapping_selenium.py
+++ scrapping_selenium.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait #allow waiting for page 
 from selenium.webdriver.support import expected_conditions as EC # determine whether the web page has loaded
 from selenium.common.exceptions import TimeoutException # handling timeout situation
 from selenium.webdriver.chrome.service import Service # This is used to pass executable path
+import time
 
 #Prepare the code for easily opening new browser window (This will be useful when we are doing parallelization)
 
@@ -20,17 +21,18 @@ try:
         driver.get("https://github.com/collections/machine-learning")
 
         #WebDriverWait.until(5)
-        projects=driver.find_elements("xpath","//h1[@class='h3 lh-condensed']")
+        projects=driver.find_elements("xpath","//h1[@class='h3 lh-condensed']/a")
         #links=driver.find_elements(By.XPATH, "/html/body/div[1]/div[5]/main/div[2]/div/div/article[1]/div[1]/h1")
         #print(projects)
         #return projects
+        time.sleep(3)
     
         project_list={}
         for proj in projects:
             proj_name=proj.text
             #proj_url=proj.find_elements("xpath","/html/body/div[1]/div[5]/main/div[2]/div/div/article[4]/div[1]/h1/a").append
-            proj_url=proj.find_elements(By.CLASS_NAME,'[h3 lh-condensed]')
-            print(proj_url)
+            proj_url=proj.get_attribute('href')
+            print(f"Project: {proj_name}, URL: {proj_url}")
             #links=[elem.get_attribute('href') for elem in proj_url]
             project_list[proj_name]=proj_url
             project_df=pd.DataFrame.from_dict(project_list,orient='index')
